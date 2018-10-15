e520acd132982ef0379f1db2fcff7ab63dddd672
feynman <feynmanmiao@auratech.hk>
2018-10-15 15:59:38 +0800

    blue

--- Dong/Dong/ViewController.m
+++ Dong/Dong/ViewController.m
@@ -17,8 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor orangeColor];
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor blueColor];
     
 }
 
